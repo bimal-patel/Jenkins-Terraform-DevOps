d1a36641d01cc4e49ed640dd37a28fd77583ef2e
bimal-patel <94635953+bimal-patel@users.noreply.github.com>
2021-11-19 12:22:12 +0530

    Added provider.tf
    Added provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region                  = "ap-south-1"
+  shared_credentials_file = "/aws/cred"
+  profile                 = "customprofile"
+}
