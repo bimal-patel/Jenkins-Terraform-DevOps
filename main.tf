16b5268e193e936234b3b6d473028e3a428eef6b
bimal-patel <94635953+bimal-patel@users.noreply.github.com>
2021-11-19 12:16:00 +0530

    updated main tf with ami id
    updated main tf with ami id

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0108d6a82a783b352"
   instance_type = "t2.micro"
 
   tags = {
