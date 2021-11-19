f67823dbd6d9bf6fc96fab52a4e0cbfc4c478dc0
bimal-patel <94635953+bimal-patel@users.noreply.github.com>
2021-11-19 11:42:23 +0530

    Save
    Save

--- main.tf
+++ main.tf
@@ -12,6 +12,8 @@ terraform {
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
+  access_key = "${AWS_ACCESS_KEY_ID}"
+  secret_key = "${AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_instance" "app_server" {
