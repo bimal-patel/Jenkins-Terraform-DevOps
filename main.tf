49ba4e1f9e77b2017ddc82e4796456c05bcc6510
bimal-patel <94635953+bimal-patel@users.noreply.github.com>
2021-11-19 12:04:53 +0530

    test
    test

--- main.tf
+++ main.tf
@@ -11,9 +11,7 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
-  access_key = "${AWS_ACCESS_KEY_ID}"
-  secret_key = "${AWS_SECRET_ACCESS_KEY}"
+  region  = "ap-south-1"
 }
 
 resource "aws_instance" "app_server" {
