a2625e8a986a367785bb5736e6645f3ae85ed8f7
bimal-patel <94635953+bimal-patel@users.noreply.github.com>
2021-11-21 18:32:33 +0530

    Added cred for s3 backend
    Added cred for s3 backend

--- provider.tf
+++ provider.tf
@@ -21,5 +21,7 @@ terraform {
     bucket = "bms-ge-tfapply-state-bucket"
     key    = "ge/terraform.tfstate"
     region = "ap-south-1"
+    shared_credentials_file = "/aws/cred"
+    profile                 = "customprofile"
   }
 }
