a6ae3694a2aefb36c12a43f119db9e5cc4f1f934
samir patel <mail.samirpatel@gmail.com>
2022-01-04 23:58:26 +0530

    added outputs

---
+++ modules/vpc/main/output.tf
@@ -0,0 +1,52 @@
+# ------------------------------------------------------------------------------
+# Output
+# ------------------------------------------------------------------------------
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = aws_vpc.main.id
+}
+
+output "cidr_block" {
+  description = "The cidr_block of the VPC."
+  value       = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "The ID of the public subnets."
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "The ID of the private subnets."
+  value       = aws_subnet.private[*].id
+}
+
+output "main_route_table_id" {
+  description = "The ID of the main route table."
+  value       = aws_vpc.main.main_route_table_id
+}
+
+output "public_subnets_route_table_id" {
+  description = "The ID of the routing table for the public subnets."
+  value       = concat(aws_route_table.public[*].id, [""])[0]
+}
+
+output "private_subnets_route_table_ids" {
+  description = "The IDs of the routing tables for the private subnets."
+  value       = aws_route_table.private[*].id
+}
+
+output "nat_gateway_ids" {
+  description = "The IDs of the NAT Gateways."
+  value       = aws_nat_gateway.private[*].id
+}
+
+output "default_security_group_id" {
+  description = "The id of the VPC default security group"
+  value       = aws_vpc.main.default_security_group_id
+}
+
+output "default_network_acl_id" {
+  description = "The ID of the network ACL created by default on VPC creation"
+  value       = aws_vpc.main.default_network_acl_id
+}
